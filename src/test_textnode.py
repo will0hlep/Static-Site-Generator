c33b7488432e9b141b4d6cbe495ab5b0a84b8250
William Paine <william@polymathsoftware.uk>
2026-02-16 13:14:59 +0000

    CH2L6

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
-
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -16,6 +15,38 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node4)
         self.assertNotEqual(node, node5)
         self.assertEqual(node5, node6)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node1 = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        node3 = TextNode("This is a text node", TextType.CODE)
+        node4 = TextNode("This is a text node", TextType.LINK, {"href": "www.boot.dev"})
+        node5 = TextNode(None, TextType.IMAGE, {
+                             "src": "www.boot.dev",
+                             "alt": "This is a text node"
+                             })
+        
+        html_node = text_node_to_html_node(node)
+        html_node1 = text_node_to_html_node(node1)
+        html_node2 = text_node_to_html_node(node2)
+        html_node3 = text_node_to_html_node(node3)
+        html_node4 = text_node_to_html_node(node4)
+        html_node5 = text_node_to_html_node(node5)
+
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node1.tag, 'b')
+        self.assertEqual(html_node2.tag, 'i')
+        self.assertEqual(html_node3.tag, 'code')
+        self.assertEqual(html_node4.tag, 'a')
+        self.assertEqual(html_node5.tag, 'img')
+
+        self.assertEqual(html_node.value, "This is a text node")
+        self.assertEqual(html_node1.value, "This is a text node")
+        self.assertEqual(html_node2.value, "This is a text node")
+        self.assertEqual(html_node3.value, "This is a text node")
+        self.assertEqual(html_node4.value, "This is a text node")
+        self.assertEqual(html_node5.value, None)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,10 +1,10 @@
 from enum import Enum
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TextType(Enum):
-    HEADING = "heading"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
-    BLOCKQUOTE = "blockquote"
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
@@ -24,3 +24,29 @@ class TextNode():
     
     def __repr__(self):
         return f'TextNode({self.text}, {self.text_type.value}, {self.url})'
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode('b', text_node.text)
+        case TextType.ITALIC:
+            return LeafNode('i', text_node.text)
+        case TextType.CODE:
+            return LeafNode('code', text_node.text)
+        case TextType.LINK:
+            return LeafNode(
+                            'a',
+                            text_node.text,
+                            {"href": text_node.url}
+                            )
+        case TextType.IMAGE:
+            return LeafNode(
+                            "img",
+                            None,
+                            {
+                             "src": text_node.url,
+                             "alt": text_node.text
+                             }
+                            )
