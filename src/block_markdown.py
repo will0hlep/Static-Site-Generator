524a8699e21a3ce3f67becb4460d031bbaa707a2
will0hlep <williamrpaine@gmail.com>
2026-02-19 11:21:52 +0000

    CH4L2

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 def markdown_to_blocks(markdown):
     output = []
     blocks = markdown.split('\n\n')
@@ -5,3 +7,43 @@ def markdown_to_blocks(markdown):
         if block != '':
             output.append(block.strip('\n').strip())
     return output
+
+
+class BlockType(Enum):
+    paragraph = "paragraph"
+    heading = "heading"
+    code = "code"
+    quote = "quote"
+    unordered_list = "unordered_list"
+    ordered_list = "ordered_list"
+
+
+def block_to_block_type(markdown):
+    if markdown[0] == "#":
+        for i in range(1,7):
+            if markdown[i] == " ":
+                return BlockType("heading")
+            if markdown[i] != "#" or i > 5:
+                break
+    elif markdown.startswith('```\n'):
+        if markdown.endswith('```'):
+            return BlockType("code")
+    else:
+        markdown_lines = markdown.split('\n')
+        num_lines = len(markdown_lines)
+        if markdown_lines[0].startswith(">"):
+            for i in range(1,num_lines):
+                if not markdown_lines[i].startswith(">"):
+                    return BlockType("paragraph")
+            return BlockType("quote")
+        if markdown_lines[0].startswith("- "):
+            for i in range(1,num_lines):
+                if not markdown_lines[i].startswith("- "):
+                    return BlockType("paragraph")
+            return BlockType("unordered_list")
+        if markdown_lines[0].startswith("1. "):
+            for i in range(1,num_lines):
+                if not markdown_lines[i].startswith(f"{i+1}. "):
+                    return BlockType("paragraph")
+            return BlockType("ordered_list")
+    return BlockType("paragraph")
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,11 +1,11 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, BlockType, block_to_block_type
 
 
 class TestMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
-        md = md = """
+        md = """
 This is **bolded** paragraph
 
         This is another paragraph with _italic_ text and `code` here
@@ -24,6 +24,98 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type(self):
+        md = """# this is a test"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('heading'))
+        md = """##!# this is a test"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('paragraph'))
+        md = """#####this is a test"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('paragraph'))
+        md = """###### this is a test"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('heading'))
+        md = """####### this is a test"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('paragraph'))
+        md = """```
+this is a test```"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('code'))
+        md = """``
+this is a test```"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('paragraph'))
+        md = """```
+this is a test``"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('paragraph'))
+        md = """> this is a test
+> this is a test
+> this is a test
+> this is a test
+> this is a test"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('quote'))
+        md = """> this is a test
+> this is a test
+ this is a test
+> this is a test
+> this is a test"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('paragraph'))
+        md = """- this is a test
+- this is a test
+- this is a test
+- this is a test
+- this is a test"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('unordered_list'))
+        md = """- this is a test
+- this is a test
+- this is a test
+ this is a test
+- this is a test"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('paragraph'))
+        md = """- this is a test
+-this is a test
+- this is a test
+- this is a test
+- this is a test"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('paragraph'))
+        md = """1. this is a test
+2. this is a test
+3. this is a test
+4. this is a test
+5. this is a test"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('ordered_list'))
+        md = """1. this is a test
+2.this is a test
+3. this is a test
+4. this is a test
+5. this is a test"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('paragraph'))
+        md = """1. this is a test
+2. this is a test
+3. this is a test
+4. this is a test
+5 this is a test"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('paragraph'))
+        md = """1. this is a test
+2. this is a test
+3. this is a test
+3. this is a test
+5. this is a test"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(blocktype, BlockType('paragraph'))
 
 
 if __name__ == "__main__":
